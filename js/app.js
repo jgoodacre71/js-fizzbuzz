d78f3561526ab7990cc3a06064e3b7a4ca705842
John Goodacre <jgoodacre71@gmail.com>
2015-06-17 12:24:40 +0100

    js fizzbuzz refactored

--- js/app.js
+++ js/app.js
@@ -1,22 +1,35 @@
 $(document).ready(function(){
 
-	var fizzbval = "TEST";
-	for (var i=1;i<=100;i++){
+	var fizzbval = prompt("Please enter an integer greater than zero");
 
-		if (i%15==0){
-			fizzbval = "fizz buzz";
-		} 
-		else if (i%3==0){
-			fizzbval = "fizz";
-		} 
-		else if (i%5==0){
-			fizzbval = "buzz";
-		}
-		else {
-			fizzbval = i;
-		}
-		$("<p>"+fizzbval+"</p>").appendTo("body");
-
+	fizzbval = +fizzbval;
+	
+	if(typeof fizzbval==='number' && (fizzbval%1)===0 && (fizzbval>0)) {
+		fizzbuzz(fizzbval);
 	}
+	else
+	{
+		alert("Sorry you need to enter an integer greater than zero")
+	}
+	
+
+	function fizzbuzz(fbval) {
+		var fizzbval = ""
+		for (var i=1;i<=fbval;i++){
+			if (i%15==0){
+				fizzbval = "fizz buzz";
+			} 
+			else if (i%3==0){
+				fizzbval = "fizz";
+			} 
+			else if (i%5==0){
+				fizzbval = "buzz";
+			}
+			else {
+				fizzbval = i;
+			}
+			$("<p>"+fizzbval+"</p>").appendTo("body");
+		}
+	}	
 
 });
